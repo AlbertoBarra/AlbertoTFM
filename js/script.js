e7bf7310d11851c3aae2223bed28ba45eb5492b7
Alberto <alberto.barra93@gmail.com>
2019-12-18 18:04:01 +0100

    RENFE_markers
    Finalizados marcadores de RENFE (con leaflet descargado)

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+// Coordenadas para el maxbound del map principal
+var corner1 = L.latLng(40.761417, -3.808321),
+    corner2 = L.latLng(40.188960, -3.006673),
+    bounds = L.latLngBounds(corner1, corner2);
+
 // Initialize the map and set its view to geographical limits of El Hierro
 var map = L.map('map', {
-  zoomControl: false,
+  zoomControl: true,
   maxZoom: 18,
-  minZoom: 0,
+  minZoom: 10,
+  maxBounds: bounds,
 }).setView([40.459707, -3.427715], 13);
 
 // Add a tile layer to the map (Mapbox Streets tile layer)
+
 var osm = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
   '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -18,11 +25,11 @@ var osm2 = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?acc
   '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
   'Imagery © <a href="http://mapbox.com">Mapbox</a>',
   id: 'mapbox.streets',
-  minZoom: 0,
-  maxZoom: 9
+  minZoom: 8,
+  maxZoom: 8
 });
 
-//Markers-------------------------------------------------------------
+//Marcadores linea de RENFE
 
 var renfeIcon = L.icon({
   iconUrl: "img/renfe.png",
@@ -32,14 +39,50 @@ var renfeIcon = L.icon({
 var renfeAtochaPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Atocha</a>"
 var renfeAtochaMarker = L.marker([40.406472, -3.689657], {icon: renfeIcon}).addTo(map).bindPopup(renfeAtochaPop)
 
+var renfeEntPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Asamblea de Madrid - Entrevias</a>"
+var renfeEntMarker = L.marker([40.381697, -3.667984], {icon: renfeIcon}).addTo(map).bindPopup(renfeEntPop)
+
+var renfePozoPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>El Pozo</a>"
+var renfePozoMarker = L.marker([40.376130, -3.656208], {icon: renfeIcon}).addTo(map).bindPopup(renfePozoPop)
+
+var renfeVkPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Vallecas</a>"
+var renfeVkMarker = L.marker([40.381937, -3.624597], {icon: renfeIcon}).addTo(map).bindPopup(renfeVkPop)
+
+var renfeSantPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Santa Eugenia</a>"
+var renfeSantMarker = L.marker([40.386891, -3.608930], {icon: renfeIcon}).addTo(map).bindPopup(renfeSantPop)
+
+var renfeVicPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Vicálvaro</a>"
+var renfeVicMarker = L.marker([40.401271, -3.595926], {icon: renfeIcon}).addTo(map).bindPopup(renfeVicPop)
+
+var renfeCosPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Coslada (apd)</a>"
+var renfeCosMarker = L.marker([40.423777, -3.561009], {icon: renfeIcon}).addTo(map).bindPopup(renfeCosPop)
+
+var renfeSFPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>San Fernando</a>"
+var renfeSFMarker = L.marker([40.442673, -3.533997], {icon: renfeIcon}).addTo(map).bindPopup(renfeSFPop)
+
 var renfeTJPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Torrejón de Ardoz</a>"
 var renfeTJMarker = L.marker([40.454773, -3.479721], {icon: renfeIcon}).addTo(map).bindPopup(renfeTJPop)
 
 var renfeSotoPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Soto del Henares</a>"
 var renfeSotoMarker = L.marker([40.463766, -3.441447], {icon: renfeIcon}).addTo(map).bindPopup(renfeSotoPop)
 
+var renfeGarPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>La Garena</a>"
+var renfeGarMarker = L.marker([40.480514, -3.393043], {icon: renfeIcon}).addTo(map).bindPopup(renfeGarPop)
+
 var renfeAHPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Alcalá de Henares</a>"
-var renfeAHMarker = L.marker([40.485640, -3.362811], {icon: renfeIcon}).addTo(map).bindPopup(renfeAHPop)
+var renfeAHMarker = L.marker([40.489025, -3.366277], {icon: renfeIcon}).addTo(map).bindPopup(renfeAHPop)
+
+var renfeAHUPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Alcalá Universidad</a>"
+var renfeAHUMarker = L.marker([40.505380, -3.335340], {icon: renfeIcon}).addTo(map).bindPopup(renfeAHUPop)
+
+var renfeMecoPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Meco</a>"
+var renfeMecoMarker = L.marker([40.534581, -3.298734], {icon: renfeIcon}).addTo(map).bindPopup(renfeMecoPop)
+
+var renfeAzqPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Azuqueca</a>"
+var renfeAzqMarker = L.marker([40.561031, -3.265453], {icon: renfeIcon}).addTo(map).bindPopup(renfeAzqPop)
+
+var renfeGuadPop = "<a href='https://www.redtransporte.com/madrid/cercanias-renfe/horarios.html'target='_blank'>Guadalajada</a>"
+var renfeGuadMarker = L.marker([40.644233, -3.182273], {icon: renfeIcon}).addTo(map).bindPopup(renfeGuadPop)
 
 //Layers-------------------------------------------------------------
 
